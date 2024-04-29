1708e021af317f8620cec8691444220f7cf8aa22
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-04-29 14:05:13 +0200

    test the prod api

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -3,5 +3,5 @@ import { Environments } from "./environment.interface";
 export const environments: Environments = {
     production: true,
     name: 'prod',
-    apiPath: 'http://localhost:4730/books'
+    apiPath: ''
 }
