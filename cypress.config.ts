63f521b9814456e48bd80656d02bf2bfad846c1d
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-04-30 13:04:00 +0200

    remove some cypress code

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
   },
   
   
-  component: {
-    devServer: {
-      framework: 'angular',
-      bundler: 'webpack',
-    },
-    specPattern: '**/*.cy.ts',
-  },
+  // component: {
+  //   devServer: {
+  //     framework: 'angular',
+  //     bundler: 'webpack',
+  //   },
+  //   specPattern: '**/*.cy.ts',
+  // },
   
 }) 
